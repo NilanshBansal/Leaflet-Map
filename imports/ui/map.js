24ff12aecc8edbe9a0b38b4e39f6c26f68a6e895
Nilansh Bansal <bansalnilansh@gmail.com>
2018-06-20 16:36:22 +0530

    added image for marker & optimization

--- imports/ui/map.js
+++ imports/ui/map.js
@@ -1,9 +1,7 @@
 import React from "react";
-
 import L from 'leaflet';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
-
 export const MAP_CENTER_COORDINATES = [51.0, 19.0];
 export const MAP_ZOOM = 4;
 export const MAP_MAX_ZOOM = 18;
@@ -16,47 +14,29 @@ const createClusterCustomIcon = function (cluster) {
     });
 };
 
-/* const redMarker = L.icon({
-    iconUrl: '../../assets/images/marker_map_icon.png',
+const redMarker = L.icon({
+    iconUrl: '/images/marker_map_icon.png',
     iconSize: [40, 40],
     iconAnchor: [20, 40],
-  }); */
+  });
+
+const markers = [
+    { position: [49.8397, 24.0297] },
+    { position: [52.2297, 21.0122] },
+    { position: [51.5074, -0.0901], popup: 'Hello world' },
+    { position: [51.5, -0.09], options: { icon: redMarker } }
+];
 
 const Map1 = () => {
     return (
         <Map className="markercluster-map" center={MAP_CENTER_COORDINATES} zoom={MAP_ZOOM} maxZoom={MAP_MAX_ZOOM}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-
             <MarkerClusterGroup
-                showCoverageOnHover={true}
-                spiderfyDistanceMultiplier={4}
-                iconCreateFunction={createClusterCustomIcon}
-                zoomToBoundsOnClick={false}
-            >
-
-                <Marker position={[49.8397, 24.0297]}>
-                    <Popup minWidth={200} closeButton={false}>
-                        <div>
-                            <b>Hello world!</b>
-                            <p>I am a lonely popup.</p>
-                        </div>
-                    </Popup>
-                </Marker>
-                <Marker position={[50.4501, 30.5234]} />
-                <Marker position={[52.2297, 21.0122]} />
-                <Marker position={[50.0647, 19.9450]} />
-                <Marker position={[48.9226, 24.7111]} />
-                <Marker position={[48.7164, 21.2611]} />
-                <Marker position={[51.5, -0.09]} />
-                <Marker position={[51.5, -0.09]} />
-                <Marker position={[51.5, -0.09]} />
-
-            </MarkerClusterGroup>
-
-
+                markers={markers}
+                onMarkerClick={(marker) => console.log(marker, marker.getLatLng())}
+                onClusterClick={(cluster) => console.log(cluster, cluster.getAllChildMarkers())}
+                onPopupClose={(popup) => console.log(popup, popup.getContent())}
+                markerOptions={{ icon: redMarker, title: 'Default title' }}
+            />
         </Map>
     );
 }
