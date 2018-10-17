2f50d68fee133186904e4cc6a34873a129ff1fe8
Nilansh Bansal <bansalnilansh@gmail.com>
2018-10-18 04:04:22 +0530

    more markers added

--- imports/ui/app.js
+++ imports/ui/app.js
@@ -12,7 +12,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome</h1>
+                <h1>Male Female Tweets Classification Clustering</h1>
                 {/* <OtherLayersExample />  */}
                 <SimpleExample />
             </div>
